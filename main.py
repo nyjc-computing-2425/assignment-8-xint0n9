fc33658cdf1cfe7d9b4e74c5afe96162aace7c1a
xint0n9.l <160615573+xint0n9@users.noreply.github.com>
2024-04-06 15:51:37 +0000

    final submission

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-# Built-in imports
+def reverse(text):
+    """
+    take in parameter which is string
+    recursive function to reverse the string
+    """
+    if len(text) == 0:
+        return ''
+    return reverse(text[1:]) + text[0]
 
+
+def is_palindrome(text):
+    """
+    check if text is a palindrome
+    """
+    if len(text) <= 1:
+        return True
+    text = text.strip(',.?!():;').upper()
+    print(text)
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
